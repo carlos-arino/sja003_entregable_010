3c81b2b30187d9aceca3162715a4bdf886116c43
carlos-arino <arino@uji.es>
2025-12-14 20:55:49 +0100

    Agregar archivos de configuración y código fuente inicial

--- src/main.cpp
+++ src/main.cpp
@@ -8,34 +8,46 @@
  DNI: XXXXX
  ENLACE WOKWI: XXXXXXX
 */
-#include <Stepper.h>
+
 #include <Arduino.h>
 
-Stepper myStepper(200, 14, 27, 26, 25);
-
-const int potPin = 34; // Pin analógico para el potenciómetro
-const int luxPin = 35; // Pin analógico para el sensor de luminosidad
+const int luxPin = 35;  // Pin analógico para el sensor de luminosidad
+const int encAPin = 23; // Pin para el canal A del encoder
+const int encBPin = 22; // Pin para el canal B del encoder
+const int encCPin = 16; // Pin para el canal C del encoder
+const int encDPin = 15; // Pin para el canal D del encoder
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Hello, ESP32!");
-  myStepper.setSpeed(10);
+  ledcAttachPin(26, 0);
+  ledcSetup(0, 1000, 10); // frecuencia 1KHz y resolucion 2^10
+  pinMode(19, INPUT);
+  pinMode(18, INPUT);
+  pinMode(5, INPUT);
+  pinMode(17, INPUT);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-
-  myStepper.step(100);
-  delay(500);
-  Serial.print(analogRead(potPin)); // siempre posición 180 del potenciómetro
+  delay(10); // this speeds up the simulation
+  if (millis() < 5000)
+    ledcWrite(0, 200);
+  if (millis() > 5000)
+    ledcWrite(0, 500);
+  if (millis() > 10000)
+    ledcWrite(0, 1000);
+  Serial.print(digitalRead(encAPin));
   Serial.print(", ");
-  Serial.println(analogRead(luxPin));
-  myStepper.step(-100);
-  Serial.print(analogRead(potPin)); // siempre posición 0 del potenciómetro
+  Serial.print(digitalRead(encBPin));
   Serial.print(", ");
-  Serial.println(analogRead(luxPin));
-  delay(500);
+  Serial.print(digitalRead(encCPin));
+  Serial.print(", ");
+  Serial.print(digitalRead(encDPin));
+  Serial.print(", ");
+  Serial.print(analogRead(luxPin));
+  Serial.println(" ");
 }
